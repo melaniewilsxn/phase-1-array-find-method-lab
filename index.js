968f96a68b6b26f3813ee015705b431b1a41186c
Melanie Wilson <melaniemwilson1@gmail.com>
2023-04-07 20:41:32 -0400

    completed lab using find()

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 // code your solution here
-function superbowlWin(record) {
-    for (let item of record){
-        if(item.result === "W"){
-            return item.year
-            break
-        }
-    }
+// function superbowlWin(record) {
+//     for (let item of record){
+//         if(item.result === "W"){
+//             return item.year
+//             break
+//         }
+//     }
+// }
+
+function superbowlWin(array) {
+    const winningObj = array.find(findWin)
+    if (winningObj === undefined){
+        return winningObj
+    } else return winningObj.year
+}
+
+function findWin (foundWin) {
+    return foundWin.result === "W"
 }
